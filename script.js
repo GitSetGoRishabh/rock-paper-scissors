08c2f3e60a4465df73a8984e30fd584d6e310552
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-06-26 17:51:37 +0530

    running game for multiple times

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function playgame(){
+    for(let i=0;i<5;i++){
+        let humanSelection = humanChoice();
+        let computerSelection =  computerChoice();
+        playRound(humanSelection,computerSelection);
+    }
+}
+
 function playRound(humanSelection,computerSelection){
     if(humanSelection==computerSelection) console.log("Tied");
     else if(
@@ -21,7 +29,4 @@ function computerChoice(){
     return choices[randomIdx];
 }
 
-let humanSelection = humanChoice();
-let computerSelection =  computerChoice();
-
-playRound(humanSelection,computerSelection);
+playgame();
